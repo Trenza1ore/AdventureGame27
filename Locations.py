853e7db13360ed29be6895acee937ecc8af5fb27
Daniel Shandley <shandleyd@cardiff.ac.uk>
2020-10-22 16:22:46 +0100

    first draft

---
+++ Locations.py
@@ -0,0 +1,21 @@
+Mafia_Bosses_Office = {
+    "name": "Mariano Giovanni's Office",
+
+    "description": "n/a"
+
+    "exits": "n/a"
+
+    "items": "n/a"
+}
+
+Warehouse = {
+    "name": "Warehouse"
+
+    "description": "n/a"
+
+    "exits": "n/a"
+
+    "items": "n/a"
+}
+
+#More locations to be added
