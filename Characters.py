40e36a437298dd30e8b8e3da18a84d4ffc641119
Razan Alghamdi <alghamdir4@cardiff.ac.uk>
2020-10-22 16:09:28 +0100

    Add new file

---
+++ Characters.py
@@ -0,0 +1,63 @@
+the_detective={
+"name":"""Det Michael (Mike) Grisham
+        The detecive"You" """,
+
+"The story": """New York, 1980s. You are a corrupt cop who recently got fired
+ You get caught up in the mafia’s drama One of the Mafiosos wants to betray the mafia family.
+ Your task is to successfully identify the traitor in a list of suspects."""
+}
+
+the_boss={"The name":"Mariano Giovanni"}
+
+
+suspect1={ "Name":"Manuele Mariano",
+
+"Who is he?": """He is the underboss
+            nephew of boss, the favourite and most likely next in line 
+            Motive: Wants to run the show faster"""}
+
+suspect2={ "Name":"Roberto Orazio",
+"Who is he?":"""Mafia associate – maybe a businessman.
+
+                Business with another family but tied into deal with them.
+                
+                Sleazy guy.
+                
+                Motive: Monopoly in drug business so he wants to take it down so other family will have monopoly and give him bigger cut."""}
+
+suspect3={"Name":"Alberto Giovanni",
+"Who is he?":"""Everyone calls him figlio.
+
+                Son of boss.
+                
+	        Apparent potential motive: Not treated with respect, could easily be won over with money.
+	        
+	        True motive: wants to undermine the boss and take over the mafia as next in line
+	        
+                Wants to pin it on the nephew.
+	        Uncover the fact that he’s the son, big reveal the son."""}
+
+suspect4={"Name":"Angelo Rosolino",
+"Who is he?":"""Consigliere.
+
+                Motive: personal grudge against the family.
+                
+	        He wants to get married to someone from the ‘ …... ‘ crime family,
+	        if he takes down this family he will be able to marry the person he loves from the other family."""}
+
+
+Characters={"Roberto Orazio":suspect2,
+            "Alberto Giovanni":suspect3,
+            "Angelo Rosolino":suspect4,
+            "Manuele Mariano":suspect1,
+            "Mariano Giovanni":the_boss,
+            "Mike":the_detective}
+            
+
+
+
+
+           
+
+
+
