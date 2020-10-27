ee2aa8863f24f8caded7b8d015e5fcb5f79049da
Daniel Shandley <shandleyd@cardiff.ac.uk>
2020-10-27 13:03:57 +0000

    Update Locations.py

--- Locations.py
+++ Locations.py
@@ -18,9 +18,21 @@ Warehouse = {
     "items": "n/a"
 }
 
+Starting_Street = {
+    "name": "a street behind the Stanky Rat Pub",
+
+    "description": "n/a",
+
+    "exits": "n/a",
+
+    "items": "n/a"
+}
+
+
 #More locations to be added
 
 All_Locations = {
     "mafia" : Mafia_Bosses_Office,
-    "warehouse" : Warehouse
+    "warehouse" : Warehouse,
+    "street" : Starting_Street
 }
